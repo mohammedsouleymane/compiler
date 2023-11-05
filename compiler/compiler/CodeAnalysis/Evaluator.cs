c6fc8647dee55cc458d4a2c15937b276a6e271d6
geegee <mohammedsouleymane@outlook.com>
2023-11-05 17:37:16 +0100

    move syntax files to new file

--- compiler/compiler/CodeAnalysis/Evaluator.cs
+++ compiler/compiler/CodeAnalysis/Evaluator.cs
@@ -1,3 +1,5 @@
+using compiler.CodeAnalysis.Syntax;
+
 namespace compiler.CodeAnalysis;
 
 public class Evaluator
--- compiler/compiler/Program.cs
+++ compiler/compiler/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using compiler.CodeAnalysis;
+using compiler.CodeAnalysis.Syntax;
 
 var showTree = false;
 while (true)
